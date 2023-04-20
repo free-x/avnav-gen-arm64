3a2992c43dbae0833f05a2a30da486117b969442
free-x <oroitburd@gmail.com>
2023-04-20 20:53:13 +0200

    cherry pick docker from @wellenvogel

--- build-docker.sh
+++ build-docker.sh
@@ -77,6 +77,13 @@ BUILD_OPTS="$(echo "${BUILD_OPTS:-}" | sed -E 's@\-c\s?([^ ]+)@-c /config@')"
 
 ${DOCKER} build --build-arg BASE_IMAGE=debian:bullseye -t pi-gen "${DIR}"
 
+CHECK_MOUNT_BINFMT_MISC=$(cat <<-END
+if [ -f /.dockerenv ] && (! grep -q "/proc/sys/fs/binfmt_misc" /proc/mounts); then
+  mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
+fi
+END
+)
+
 if [ "${CONTAINER_EXISTS}" != "" ]; then
 	trap 'echo "got CTRL+C... please wait 5s" && ${DOCKER} stop -t 5 ${CONTAINER_NAME}_cont' SIGINT SIGTERM
 	time ${DOCKER} run --rm --privileged \
@@ -88,7 +95,7 @@ if [ "${CONTAINER_EXISTS}" != "" ]; then
 		-e "GIT_HASH=${GIT_HASH}" \
 		--volumes-from="${CONTAINER_NAME}" --name "${CONTAINER_NAME}_cont" \
 		pi-gen \
-		bash -e -o pipefail -c "dpkg-reconfigure qemu-user-static &&
+		bash -e -o pipefail -c "dpkg-reconfigure qemu-user-static && $CHECK_MOUNT_BINFMT_MISC &&
 	# binfmt_misc is sometimes not mounted with debian bullseye image
 	(mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc || true) &&
 	cd /pi-gen; ./build.sh ${BUILD_OPTS} &&
@@ -104,7 +111,7 @@ else
 		--volume "${CONFIG_FILE}":/config:ro \
 		-e "GIT_HASH=${GIT_HASH}" \
 		pi-gen \
-		bash -e -o pipefail -c "dpkg-reconfigure qemu-user-static &&
+		bash -e -o pipefail -c "dpkg-reconfigure qemu-user-static && $CHECK_MOUNT_BINFMT_MISC &&
 	# binfmt_misc is sometimes not mounted with debian bullseye image
 	(mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc || true) &&
 	cd /pi-gen; ./build.sh ${BUILD_OPTS} &&
